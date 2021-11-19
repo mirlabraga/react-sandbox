a4ab3c8748cc66e2f30979c622e2feb75fcff03e
Mirla Chucre <mirla.chucre@thoughtworks.com>
2021-11-19 10:10:23 +0000

    Add login button with redirect with auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Auth0Client } from "@auth0/auth0-spa-js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Auth0Callback(){
+  return (<></>);
+}
+const auth0 = new Auth0Client({
+  domain: 'dev-q7rj5o44.us.auth0.com',
+  client_id: 'PH0OBJ2fYk1bc3akSovlIe03jwoi0bqa',
+  scope: 'openid profile email'
+})
+
+function Home(){
+
+  const login = async () => {
+    await auth0.loginWithRedirect();
+  }
+
+  return (<><button onClick={login}>login</button></>);
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/callback" element={<Auth0Callback />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
